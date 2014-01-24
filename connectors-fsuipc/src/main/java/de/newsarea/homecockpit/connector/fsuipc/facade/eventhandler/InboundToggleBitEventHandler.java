21bfbc44b0daffff60e2e6b10176d244d28e38fe
Robert Bernhardt <bernhardt@newsarea.de>
2014-01-24 18:22:48 +0100

    add direct toggleBit function to InboundToggleBitEventHandler

--- connectors-fsuipc/src/main/java/de/newsarea/homecockpit/connector/fsuipc/facade/eventhandler/InboundToggleBitEventHandler.java
+++ connectors-fsuipc/src/main/java/de/newsarea/homecockpit/connector/fsuipc/facade/eventhandler/InboundToggleBitEventHandler.java
@@ -3,15 +3,12 @@ package de.newsarea.homecockpit.connector.fsuipc.facade.eventhandler;
 import de.newsarea.homecockpit.connector.facade.eventhandler.InboundEventHandler;
 import de.newsarea.homecockpit.connector.fsuipc.FSUIPCConnector;
 import de.newsarea.homecockpit.connector.fsuipc.facade.eventhandler.domain.FSUIPCOffset;
-import de.newsarea.homecockpit.fsuipc.domain.OffsetIdent;
-import de.newsarea.homecockpit.fsuipc.domain.OffsetItem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.AbstractMap;
 import java.util.Map;
-import java.util.concurrent.TimeoutException;
 
 public class InboundToggleBitEventHandler extends AbstractFSUIPCEventHandler implements InboundEventHandler {
 	
@@ -43,24 +40,7 @@ public class InboundToggleBitEventHandler extends AbstractFSUIPCEventHandler imp
     @Override
     public void handleInboundEvent(Object value) throws IOException {
 		log.debug("handleOutput - {}", value);
-        try {
-            toggleBit(getOffset().getValue(), getSize(), getBitIdx());
-        } catch (TimeoutException e) {
-            log.error(e.getMessage(), e);
-        }
-    }
-	
-	/* HELPER */
-
-    private void toggleBit(int offset, int size, byte bitIdx) throws IOException, TimeoutException {
-    	OffsetItem offsetItem = getConnector().read(new OffsetIdent(offset, size));
-        byte[] value = offsetItem.getValue().toByteArray();
-    	int bidx = (bitIdx / 8);
-		byte nidx = (byte)(bitIdx % 8);
-		byte newByteValue = value[value.length - 1 - bidx];
-		newByteValue ^= (1 << nidx);
-		value[value.length - 1 - bidx] = newByteValue;
-        getConnector().write(new OffsetItem(offset, size, value));
+        getConnector().toggleBit(getOffset().getValue(), getSize(), getBitIdx());
     }
 
 }
