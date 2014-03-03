cb5734df3f224c14dbfc2b91b820b2aec6c34b58
Robert Bernhardt <bernhardt@newsarea.de>
2014-03-03 17:49:54 +0100

    change HeadingConverter from int to long

--- connectors-fsuipc/src/main/java/de/newsarea/homecockpit/connector/fsuipc/facade/eventhandler/converter/HeadingConverter.java
+++ connectors-fsuipc/src/main/java/de/newsarea/homecockpit/connector/fsuipc/facade/eventhandler/converter/HeadingConverter.java
@@ -1,13 +1,12 @@
 package de.newsarea.homecockpit.connector.fsuipc.facade.eventhandler.converter;
 
-import com.google.common.primitives.Ints;
 import de.newsarea.homecockpit.fsuipc.util.FSUIPCUtil;
 
 public class HeadingConverter implements ValueConverter<Long, Double> {
 
 	@Override
 	public Double toInput(Long data) {
-		return FSUIPCUtil.toHeading(Ints.checkedCast(data));
+		return FSUIPCUtil.toHeading(data);
 	}
 
 	@Override
