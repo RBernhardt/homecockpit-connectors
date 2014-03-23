e136454b48376b77fc395fa1517fd9a7b3a4be49
Robert Bernhardt <bernhardt@newsarea.de>
2014-03-23 02:37:47 +0100

    add equals and hashcode

--- connectors-fsuipc/src/main/java/de/newsarea/homecockpit/connector/fsuipc/facade/eventhandler/domain/AirplanePosition.java
+++ connectors-fsuipc/src/main/java/de/newsarea/homecockpit/connector/fsuipc/facade/eventhandler/domain/AirplanePosition.java
@@ -48,7 +48,42 @@ public class AirplanePosition {
 		this.speed = speed;
 	}
 
-	@Override
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AirplanePosition)) return false;
+
+        AirplanePosition that = (AirplanePosition) o;
+
+        if (Double.compare(that.altitude, altitude) != 0) return false;
+        if (bank != that.bank) return false;
+        if (heading != that.heading) return false;
+        if (Double.compare(that.latitude, latitude) != 0) return false;
+        if (Double.compare(that.longitude, longitude) != 0) return false;
+        if (pitch != that.pitch) return false;
+        if (speed != that.speed) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        temp = Double.doubleToLongBits(latitude);
+        result = (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(longitude);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(altitude);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        result = 31 * result + pitch;
+        result = 31 * result + bank;
+        result = 31 * result + heading;
+        result = 31 * result + speed;
+        return result;
+    }
+
+    @Override
 	public String toString() {
 		return "AirplanePosition [latitude=" + latitude + ", longitude=" + longitude + ", altitude=" + altitude + ", pitch=" + pitch + ", bank="
 				+ bank + ", heading=" + heading + ", speed=" + speed + "]";
